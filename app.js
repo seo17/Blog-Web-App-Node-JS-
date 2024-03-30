be7ba92029aec20bc2295c906f5a465cd9ab97a5
seo17 <ebenmosi17@gmail.com>
2024-03-30 23:07:48 +0100

    Add post route to view individual post

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import _ from "lodash";
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -48,6 +49,20 @@ app.post('/compose', (req, res) => {
   res.redirect('/');
 });
 
+
+app.get('/posts/:postName', (req, res) => {
+  let requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach((post) => {
+    let storedTitle = _.lowerCase(post.title);
+    
+    if(requestedTitle === storedTitle){
+      res.render('post', {title: post.title, content: post.content});
+    } else {
+      console.log("match not found");
+    }
+  });
+});
 
 
 
